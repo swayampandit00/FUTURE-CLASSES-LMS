dea970ccef8616a3b0438abbb20525456ce88453
Swayam upadhyay <149935246+swayampandit00@users.noreply.github.com>
2025-12-19 13:21:32 +0530

    Change axios base URL to production server
    backend url

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://futureclasses.onrender.com",//"http://localhost:5000",
 });
 
 axiosInstance.interceptors.request.use(
